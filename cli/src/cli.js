26f269ec8a6c4ecdc5cce935bb7400005450a026
nicholashudson2 <nicholas.hudson2@yahoo.com>
2017-09-07 22:37:48 -0500

    Host & Port arguments updated & tested. Still tracing null pointer exception in users command. Colors working as intended.

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -7,24 +7,44 @@ export const cli = vorpal()
 
 let username
 let server
-let commands = ['echo', 'broadcast']
-let currCommand = null
+let commands = ['echo', 'broadcast', 'connect', 'disconnect', 'users']
+let prevCommand = ''
+let useContents = ''
+let hostAdd = 'localhost'
+let portAdd = 8080
 
 cli
   .delimiter(cli.chalk['yellow']('ftd~$'))
 
 cli
-  .mode('connect <username>')
+  .mode('connect <username> <host> <port>')
   .delimiter(cli.chalk['green']('connected>'))
   .init(function (args, callback) {
     username = args.username
-    server = connect({ host: 'localhost', port: 8080 }, () => {
+    if(args.host !== null && args.port !== null) {
+      hostAdd = args.host 
+      portAdd = args.port 
+    } 
+    console.log(hostAdd + ' : ' + portAdd)
+    server = connect({ host: hostAdd, port: portAdd }, () => {
       server.write(new Message({ username, command: 'connect' }).toJSON() + '\n')
       callback()
     })
-    
+
     server.on('data', (buffer) => {
-      this.log(buffer.toString())
+      let message = buffer.toString()
+      if (message.includes('\(all\)'))
+        this.log(cli.chalk['blue'](message))
+      if (message.includes('\(whisper\)'))
+        this.log(cli.chalk['yellow'](message))
+      if (message.includes('\(echo\)'))
+        this.log(cli.chalk['gray'](message))
+      if (message.includes('disconnected'))
+        this.log(cli.chalk['red'](message))
+      if (message.includes('has connected'))
+        this.log(cli.chalk['magenta'](message))
+      if(message.includes('currently connected'))
+        this.log(cli.chalk['cyan'](message))
     })
 
     server.on('end', () => {
@@ -35,14 +55,13 @@ cli
     const [command, ...rest] = input.split(" ")
     const contents = rest.join(' ')
     const whisperCmd = /\@[^\s]+/;
-
+    
     if (command === 'disconnect' || command === 'users') {
-      server.end(new Message({ username, command, contents: 'noContents' }).toJSON() + '\n')
+      server.end(new Message({ username, command }).toJSON() + '\n')
     } else if (command === 'echo' || command === 'broadcast' || whisperCmd.test(command)) {
       server.write(new Message({ username, command, contents }).toJSON() + '\n')
     } else {
       this.log(`Command <${command}> was not recognized`)
     }
-
     callback()
   })
